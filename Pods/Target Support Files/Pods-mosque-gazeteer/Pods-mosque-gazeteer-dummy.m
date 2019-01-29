4f02f0d98724b6ec4ae1fd00ad859e8bb8116655
Haris Khaliq <haris.khaliq@thescore.com>
2019-01-28 23:13:39 -0500

    Delete pods

--- Pods/Target Support Files/Pods-mosque-gazeteer/Pods-mosque-gazeteer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mosque_gazeteer : NSObject
-@end
-@implementation PodsDummy_Pods_mosque_gazeteer
-@end
--- Pods/Target Support Files/Pods-mosque-gazeteerTests/Pods-mosque-gazeteerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mosque_gazeteerTests : NSObject
-@end
-@implementation PodsDummy_Pods_mosque_gazeteerTests
-@end
