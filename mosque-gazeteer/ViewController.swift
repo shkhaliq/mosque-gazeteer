0cb2d54eb07e090f0f5e68feb38deb200aed1ab5
Haris Khaliq <haris.khaliq@thescore.com>
2019-01-28 22:58:03 -0500

    Initial Commit

---
+++ mosque-gazeteer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mosque-gazeteer
+//
+//  Created by Haris Khaliq on 2019-01-28.
+//  Copyright © 2019 HarisKhaliq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ mosque-gazeteerTests/mosque_gazeteerTests.swift
@@ -0,0 +1,34 @@
+//
+//  mosque_gazeteerTests.swift
+//  mosque-gazeteerTests
+//
+//  Created by Haris Khaliq on 2019-01-28.
+//  Copyright © 2019 HarisKhaliq. All rights reserved.
+//
+
+import XCTest
+@testable import mosque_gazeteer
+
+class mosque_gazeteerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
