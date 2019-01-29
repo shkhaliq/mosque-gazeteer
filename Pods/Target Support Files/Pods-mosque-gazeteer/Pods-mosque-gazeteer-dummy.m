674929f2b9598df7ebb9a4074574dc4a628c26d2
Haris Khaliq <haris.khaliq@thescore.com>
2019-01-28 23:00:46 -0500

    Integrate Pods

---
+++ Pods/Target Support Files/Pods-mosque-gazeteer/Pods-mosque-gazeteer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mosque_gazeteer : NSObject
+@end
+@implementation PodsDummy_Pods_mosque_gazeteer
+@end
---
+++ Pods/Target Support Files/Pods-mosque-gazeteerTests/Pods-mosque-gazeteerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mosque_gazeteerTests : NSObject
+@end
+@implementation PodsDummy_Pods_mosque_gazeteerTests
+@end
