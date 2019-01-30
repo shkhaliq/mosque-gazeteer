e7aa167f5e2a2485d41271a7b615abf4d291752d
Haris Khaliq <haris.khaliq@thescore.com>
2019-01-30 09:13:50 -0500

    Removes main storyboard and adds an ApplicationLaunchService

---
+++ mosque-gazeteer/App Delegate Services/ApplicationLaunchService.swift
@@ -0,0 +1,22 @@
+//
+//  ApplicationLaunchService.swift
+//  mosque-gazeteer
+//
+//  Created by Haris Khaliq on 2019-01-30.
+//  Copyright © 2019 HarisKhaliq. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationLaunchService: NSObject, ApplicationService {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
